0c1527e8a2aca7636f2609541ead30192439f718
kyle-playground <yungkailiu@gmail.com>
2021-08-21 14:34:55 +0800

    fix the bug having wrong output size in post fc layer

--- model.py
+++ model.py
@@ -51,20 +51,23 @@ class ComplexInputNetworkandCentrailzedCritic(TorchModelV2, nn.Module):
         self.action_space = action_space
         # Build the CNN(s) given obs_space's image components.
         self.cnns = {}
+        self.vf_cnns = {}
+
         self.one_hot = {}
         self.flatten = {}
         self.n_agents = 3
         concat_size = 0
+        vf_concat_size = 0
 
         for i, component in enumerate(self.original_space):
             # Image space.
-            if len(component.shape) == 3 and i == 0:
+            if len(component.shape) == 3:
                 config = {
                     "conv_filters": model_config["conv_filters"]
                     if "conv_filters" in model_config else
                     get_filter_config(obs_space.shape),
                     "conv_activation": model_config.get("conv_activation"),
-                    "post_fcnet_hiddens": [128, 128],
+                    "post_fcnet_hiddens": [],
                 }
                 cnn = ModelCatalog.get_model_v2(
                     component,
@@ -73,11 +76,16 @@ class ComplexInputNetworkandCentrailzedCritic(TorchModelV2, nn.Module):
                     model_config=config,
                     framework="torch",
                     name="cnn_{}".format(i))
+                # obs[0] for policy, obs[2] for centralized critic
+                if i == 0:
+                    concat_size += cnn.num_outputs * 4  # channel x final output shape(2x2)
+                    self.cnns[i] = cnn
+                    self.add_module("cnn_all_channel", cnn)
+                elif i == 2:
+                    vf_concat_size = cnn.num_outputs * 4
+                    self.vf_cnns[i] = cnn
+                    self.add_module("cnn_global_critic", cnn)
 
-                # TODO: why multiply 4....??????
-                concat_size += cnn.num_outputs * 4
-                self.cnns[i] = cnn
-                self.add_module("cnn_all_channel", cnn)
             # Discrete inputs -> One-hot encode.
             elif isinstance(component, Discrete):
                 self.one_hot[i] = True
@@ -88,12 +96,21 @@ class ComplexInputNetworkandCentrailzedCritic(TorchModelV2, nn.Module):
                 concat_size += self.flatten[i]
 
         # Optional post-concat FC-stack.
-        # post_fc_numout = 64
-        self.post_fc_stack = SlimFC(
-            in_size=concat_size,
-            out_size=model_config["post_fcnet_hiddens"][0],
-            activation_fn="relu",
-            initializer=torch_normc_initializer(0.01))
+        post_fc_stack_config = {
+            "fcnet_hiddens": model_config.get("post_fcnet_hiddens", [128, 128]),
+            "fcnet_activation": model_config.get("post_fcnet_activation",
+                                                 "relu")
+        }
+        self.post_fc_stack = ModelCatalog.get_model_v2(
+            Box(float("-inf"),
+                float("inf"),
+                shape=(concat_size, ),
+                dtype=np.float32),
+            self.action_space,
+            None,
+            post_fc_stack_config,
+            framework="torch",
+            name="post_fc_stack")
 
         # Actions and value heads.
         self.logits_layer = None
@@ -103,49 +120,23 @@ class ComplexInputNetworkandCentrailzedCritic(TorchModelV2, nn.Module):
         if num_outputs:
             # Action-distribution head.
             self.logits_layer = SlimFC(
-                in_size=model_config["post_fcnet_hiddens"][0],
+                in_size=self.post_fc_stack.num_outputs,
                 out_size=num_outputs,
                 activation_fn=None,   # TODO: check should I use softmax
             )
             # Create the value branch model.
             self.value_layer = SlimFC(
-                in_size=model_config["post_fcnet_hiddens"][0],
+                in_size=self.post_fc_stack.num_outputs,
                 out_size=1,
                 activation_fn=None,
                 initializer=torch_normc_initializer(0.01))
         else:
             self.num_outputs = concat_size
 
-        # Central VF maps (global obs, acts) -> vf_pred
-        self.vf_cnns = {}
-        self.vf_one_hot = {}
-        self.vf_flatten = {}
-        vf_concat_size = 0
-
-        vf_cnn_config = {
-            "conv_filters": model_config["conv_filters"]
-            if "conv_filters" in model_config else
-            get_filter_config(obs_space.shape),
-            "conv_activation": model_config.get("conv_activation"),
-            "post_fcnet_hiddens": [],
-        }
-
-        global_state_channel = 3
-        vf_cnn = ModelCatalog.get_model_v2(
-            Box(low=0, high=2, shape=[24, 24, global_state_channel]),
-            action_space,
-            num_outputs=None,
-            model_config=vf_cnn_config,
-            framework="torch",
-            name="vf_cnn_global")
-
-        # TODO: why multiply 4....??????
-        vf_concat_size += vf_cnn.num_outputs * 4
-        self.vf_cnns[0] = vf_cnn
-        self.add_module("vf_cnn_global_state", vf_cnn)
+        # Centralized critic post fc layer
         # one-shot (Discrete action space)
         vf_concat_size += 5 * self.n_agents
-        """
+
         self.vf_post_fc_stack = ModelCatalog.get_model_v2(
             Box(float("-inf"),
                 float("inf"),
@@ -156,15 +147,9 @@ class ComplexInputNetworkandCentrailzedCritic(TorchModelV2, nn.Module):
             post_fc_stack_config,
             framework="torch",
             name="vf_post_fc_stack")
-        """
-        self.vf_post_fc_stack = SlimFC(
-                in_size=vf_concat_size,
-                out_size=model_config["post_fcnet_hiddens"][0],
-                activation_fn="relu",
-                initializer=torch_normc_initializer(0.01))
 
         self.central_vf_layer = SlimFC(
-                in_size=model_config["post_fcnet_hiddens"][0],
+                in_size=self.vf_post_fc_stack.num_outputs,
                 out_size=1,
                 activation_fn=None,
                 initializer=torch_normc_initializer(0.01))
@@ -176,7 +161,6 @@ class ComplexInputNetworkandCentrailzedCritic(TorchModelV2, nn.Module):
         for i, component in enumerate(input_dict["obs"]):
             if i in self.cnns:   # use in because self.cnns is a dict
                 cnn_out, _ = self.cnns[i]({"obs": component})
-                # TODO: why cnn_out has a dimension of 128 (32x4)
                 outs.append(cnn_out)
             elif i in self.flatten:
                 outs.append(torch.reshape(component, [-1, self.flatten[i]]))
@@ -184,7 +168,7 @@ class ComplexInputNetworkandCentrailzedCritic(TorchModelV2, nn.Module):
         # Concat all outputs and the non-image inputs.
         out = torch.cat(outs, dim=1)
         # Push through (optional) FC-stack (this may be an empty stack).
-        out = self.post_fc_stack(out)
+        out, _ = self.post_fc_stack({"obs": out}, [], None)
 
         # No logits/value branches.
         if self.logits_layer is None:
@@ -198,14 +182,16 @@ class ComplexInputNetworkandCentrailzedCritic(TorchModelV2, nn.Module):
     def central_value_function(self, obs, action, other_actions):
         original_obs = restore_original_dimensions(obs, self.original_space, "torch")
         outs = []
-        vf_cnn_out, _ = self.vf_cnns[0]({"obs": original_obs[2]})
+
+        vf_cnn_out, _ = self.vf_cnns[2]({"obs": original_obs[2]})
         outs.append(vf_cnn_out)
         flatten_self_acts = one_hot(action, self.action_space)
         outs.append(flatten_self_acts)
         flatten_other_acts = one_hot(other_actions, self.action_space).reshape(original_obs[0].size()[0], (self.n_agents-1) * 5)
         outs.append(flatten_other_acts)
+
         out = torch.cat(outs, dim=1)
-        out = self.vf_post_fc_stack(out)
+        out, _ = self.vf_post_fc_stack({"obs": out}, [], None)
         central_value = self.central_vf_layer(out)
         return torch.reshape(central_value, [-1])
 
